0abe0f9f9d8ae16e27bd10a38004b036561a6a01
Yan Evtimov <ysevtimov@gmail.com>
2019-03-18 23:15:56 +0200

    added some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './main/welcome/welcome.component';
+import { AchievementsComponent } from './main/achievements/achievements.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { ProjectsComponent } from './main/projects/projects.component';
+import { FormComponent } from './main/form/form.component';
+import { ContactsComponent } from './main/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    WelcomeComponent,
+    AchievementsComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FormComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
