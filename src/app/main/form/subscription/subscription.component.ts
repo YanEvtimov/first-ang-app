702ba1af2ae0d3f43053d37d51a63c1028d68ca9
Yan Evtimov <ysevtimov@gmail.com>
2019-04-08 15:51:32 +0300

    added animation on view subscribers link

--- src/app/main/form/subscription/subscription.component.ts
+++ src/app/main/form/subscription/subscription.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { StoreDataService } from 'src/app/services/server.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
   styleUrls: ['./subscription.component.css'],
-  styles: [':host {width: 100%;}']
+  styles: [':host {width: 100%;}'],
+  animations: [
+    trigger('viewSubscibersAnimation', [
+      state('open', style({
+        opacity: 1,
+        height: '100%'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          height: '0px'
+        }),
+        animate(500)
+      ]),
+    ])
+  ]
 })
 export class SubscriptionComponent implements OnInit {
   data: any = {};
@@ -28,7 +44,7 @@ export class SubscriptionComponent implements OnInit {
     this.singupForm.reset();
     alert('Thank you for your subscription, ' + this.data.name + ', you can now add your own project templates!');
   }
-  
+
   getSubscribers() {
     this.storeDataService.getData()
       .subscribe(
